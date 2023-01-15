8fb70cf4ba39ae6047a6676d8e55201bcc1a0421
abdulmaleekadams <harbdulmaleek@gmail.com>
2023-01-15 15:47:37 -0800

    Add lotto logic

--- src/components/lottery/Lottery.js
+++ src/components/lottery/Lottery.js
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
 import Ball from "./Ball";
 import "./lottery.css";
+
 const Lottery = ({ title = "Lotto", maxNum = 30, maxBalls = 6 }) => {
-  const [nums, setNums] = useState([]);
+  const [nums, setNums] = useState(Array.from({ length: maxBalls }, () => 0));
+
+  const handleClick = () => {
+    setNums(
+      Array.from(
+        { length: maxBalls },
+        () => Math.floor(Math.random() * maxNum) + 1
+      )
+    );
+  };
+
   return (
     <div className="lottery">
       <h1>{title}</h1>
       <div className="ballContainer">
-        <Ball num={8} />
-        <Ball num={8} />
-        <Ball num={8} />
-        <Ball num={8} />
-        <Ball num={8} />
+        {nums.map((n, i) => (
+          <Ball key={i} num={n} />
+        ))}
       </div>
+      <button onClick={handleClick}>Generate</button>
     </div>
   );
 };
